d6b4e96469509894f05574011fb351fc49dd1b72
astghik-dev <astghiksaghyan@yahoo.com>
2017-08-02 15:52:45 +0300

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { LogeInFormComponent } from './loge-in-form/loge-in-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogeInFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
